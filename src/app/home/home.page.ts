f58fe1038208286019f7c2ac80f69596e7fde8c7
Diego Gómez González <93382813+diegogomezgonza@users.noreply.github.com>
2024-04-23 16:59:41 +0200

    Añadir y eliminar imagen + iconos para demás acciones

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  selectedImage: string | ArrayBuffer = ''; // Variable para almacenar la imagen seleccionada
 
   constructor() {}
 
+  // Método que se ejecuta cuando se selecciona un archivo
+  onFileSelected(event: any) {
+    const file = event.target.files[0]; // Obtiene el archivo seleccionado
+    if (file) {
+      const reader = new FileReader(); // Crea un objeto FileReader
+      reader.onload = (e: any) => {
+        this.selectedImage = e.target.result; // Almacena la imagen seleccionada en la variable
+      };
+      reader.readAsDataURL(file); // Lee el contenido del archivo como URL
+    }
+  }
+
+  // Método para eliminar la imagen seleccionada
+  deleteImage() {
+    this.selectedImage = ''; // Vacía la variable que contiene la imagen
+  }
 }
