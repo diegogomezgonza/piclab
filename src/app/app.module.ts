1c406e3f597c48949358a6d97fa06ebfd0d135ac
Diego Gómez González <93382813+diegogomezgonza@users.noreply.github.com>
2024-04-23 22:15:02 +0200

    Fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,9 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { environment } from '../environments/environment';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFireAuthModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  firebaseConfig : {
-    apiKey: "AIzaSyBi4pWDAyUMkMVPwmLvxYnzQ0a9C8BHRR0",
-    authDomain: "piclab-5af19.firebaseapp.com",
-    projectId: "piclab-5af19",
-    storageBucket: "piclab-5af19.appspot.com",
-    messagingSenderId: "763902693289",
-    appId: "1:763902693289:web:a6d5081a6ed9c9b29a3eb8"
-  },
-  production: false,
+  production: true
 };
